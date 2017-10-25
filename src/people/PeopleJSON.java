c18cdef0129031f0a4a4b3ab3bbb9c3e4bc0bd8f
chany93 <giovannirafael.vuolo@studenti.unitn.it>
2017-10-25 22:10:37 +0200

    Update PeopleJSON.java

--- src/people/PeopleJSON.java
+++ src/people/PeopleJSON.java
@@ -16,15 +16,17 @@ public class PeopleJSON {
 
 	public static void initializeDB() {
 		
+		//activities to add to the persons
 		Activity a1 = new Activity(1, "Knowledge", "Knows nothing", "Westeros", "2016-11-13");
 		Activity a2 = new Activity(2, "Killing", "Killing people", "Westeros", "2017-11-11");
 		Activity a3 = new Activity(3, "Swimming", "Swimming and winning", "USA", "2017-12-15");
+		
+		//person to write in json
 		People Jon = new People(new Long(1), "Jon", "Snow", "1985-07-26", a1);
-		
 		People Arya = new People(new Long(2), "Arya", "Stark", "1984-08-02", a2);
-		
 		People Michele = new People(new Long(3), "Michele", "Felpe", "1984-09-20", a3);
-
+		
+		//adding the persons to the instance of the annotation
 		people.getData().add(Jon);
 		people.getData().add(Arya);
 		people.getData().add(Michele);

c83c3fe7d73f63ab1f9208ca7f2124bbb4799087
chany93 <giovannirafael.vuolo@studenti.unitn.it>
2017-10-25 22:30:02 +0200

    Update Activity.java

--- src/model/Activity.java
+++ src/model/Activity.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Calendar;
-
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
@@ -18,17 +16,8 @@ public class Activity {
 	private String startDate;
 	
 
-	public Activity(String name, String description) {
-		this.name = name;
-		this.description = description;
-	}
-
 	public Activity() {
-		this.activityId = 1;
-		this.name = "Noname this time";
-		this.description = "busy doing my favourite activity";
-		this.setPlace("on the Earth");
-		this.startDate = this.getRandomDate();
+		
 	}
 	
 	public Activity(int id, String name, String description, String place, String startDate) {
@@ -79,28 +68,5 @@ public class Activity {
 	public void setStartDate(String startDate) {
 		this.startDate = startDate;
 	}
-	private String getRandomDate() {
-		int currentYear = Calendar.getInstance().get(Calendar.YEAR); 		// 1. get the current year
-		int year = (int) Math.round(Math.random()*(currentYear-1950)+1950); // 2. generate a random year 
-																			//    between 1950 and currentYear
-		int month = (int) Math.round(Math.floor(Math.random()*11)+1);		// 3. select a random month of the year
-		// 4. select a random day in the selected month
-		// 4.1 prepare a months array to store how many days in each month
-		int[] months = new int[]{31,28,30,30,31,30,31,31,30,31,30,31};
-		// 4.2 if it is a leap year, feb (months[1]) should be 29
-		if ((currentYear % 4 == 0) && ((currentYear % 100 != 0) || (currentYear % 400 == 0))) {
-			months[1] = 29;
-		}
-		long day = Math.round(Math.floor(Math.random()*(months[month-1]-1)+1));
-		return ""+year+"-"+month+"-"+day;
-	}
-
-	// Modify to String to add the missing attributes.
-	public String toString() {
-		return "Name="+name+", Description="+description + "Place="+place+"StartDate="+startDate;
-	}
-
-
-
 
 }
